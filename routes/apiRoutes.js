6d9f895516d61f6fa87c7b87a2d112640dc3769e
Pablo Ontiveros <pablacas@gmail.com>
2020-08-27 03:53:09 -0500

    started api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+var notes = require("../db/db.json")
+
+
+
+
+app.get("/api/notes", function (req, res) {
+
+});
+
+
+app.post("/api/notes", function (req, res) {
+
+});
+
+
+app.delete("api/notes/:id", function (req, res) {
+
+
+});
