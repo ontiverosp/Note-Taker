c83b82dc93d5a35ff8fe431dd88e547108a8c559
Pablo Ontiveros <pablacas@gmail.com>
2020-08-28 01:45:51 -0500

    post route adds id and sends a response to confirm

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,12 @@ module.exports = function (app) {
 
     app.post("/api/notes", function (req, res) {
         var newNote = req.body;
+        var id = Date.now();
+        // console.log("pre id: "+JSON.stringify(newNote));
+        newNote.id = id;
+        // console.log("post id: "+JSON.stringify(newNote));
         notes.push(newNote);
+        res.send('POST request complete')
     });
 
 
