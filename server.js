19c1e80ab43fa49f86cfe4591f0a2a8a25873b9d
Pablo Ontiveros <pablacas@gmail.com>
2020-08-28 02:56:38 -0500

    final fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 app.listen(PORT, function() {
