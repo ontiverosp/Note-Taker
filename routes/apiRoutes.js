618ddd24a294601aa323ee2c8de85ed09c6b2635
Pablo Ontiveros <pablacas@gmail.com>
2020-08-28 02:48:15 -0500

    used fs to actually re write db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
-var notes = require("../db/db.json")
+var notes = require("../db/db.json");
+const fs = require("fs");
+const path = require("path");
 
-
+const OUTPUT_DIR = path.resolve(__dirname, "../db");
+const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
@@ -11,24 +14,21 @@ module.exports = function (app) {
     app.post("/api/notes", function (req, res) {
         var newNote = req.body;
         var id = Date.now();
-        // console.log("pre id: "+JSON.stringify(newNote));
         newNote.id = id;
-        // console.log("post id: "+JSON.stringify(newNote));
         notes.push(newNote);
-        res.send('POST request complete')
+        fs.writeFileSync(outputPath, JSON.stringify(notes));
+        res.send(newNote)
     });
 
 
     app.delete("/api/notes/:id", function (req, res) {
         var toDelete = req.params.id;
-        // console.log(toDelete);
         for (i=0; i<notes.length ; i++) {
-            // console.log(notes[i].id)
             if (notes[i].id == toDelete){
-                // console.log("one match" + JSON.stringify(notes[i]))
                 notes.splice(i, 1);
             } 
         }
+        fs.writeFileSync(outputPath, JSON.stringify(notes));
         res.send('Delete request complete')
     });
 
